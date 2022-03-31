a987e2d00a5ad7f2c7a4786ac1f14a277975b11d
axinzwz <101556863+axinzwz@users.noreply.github.com>
2022-03-31 19:22:04 +0800

    Add files via upload

---
+++ arctan.py
@@ -0,0 +1,37 @@
+#import sys
+#import math
+from math import fabs
+from math import pi
+def atan(x):
+    #x = input()
+    #x = float(x)
+    if fabs(x) < 1:  # 输入值绝对值小于1时，采用泰勒级数展开
+        g = x
+        t = x
+        n = 1
+        while (n < 9999):
+            t = ((-1) ** n) * (x ** (2 * n + 1)) / (2 * n + 1)
+            g += t
+            n += 1
+    else:  # 输入值绝对值大于1，用 pi/2 - atan(x)
+        x = 1 / x
+        g = x
+        t = x
+        n = 1
+        while (n < 9999):
+            t = ((-1) ** n) * (x ** (2 * n + 1)) / (2 * n + 1)
+            g += t
+            n += 1
+        g = pi / 2 - g
+
+    if x >= 0:
+        g = round(g / pi * 180, 2)
+
+    elif x < 0:
+        if g >= 0:
+            g = round((g - pi) / pi * 180, 2)
+        else:
+            g = g + pi
+            g = round((g - pi) / pi * 180, 2)
+    #print(g)
+    return g
